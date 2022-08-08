2b1915907ab7f6990e140154878d0633de81a99e
이효곤 <LeeHyogon>
2022-08-08 18:11:30 +0900

    First

---
+++ src/main/java/gg/Lolog/LologApplication.java
@@ -0,0 +1,13 @@
+package gg.Lolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LologApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gg/Lolog/LologApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.Lolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
